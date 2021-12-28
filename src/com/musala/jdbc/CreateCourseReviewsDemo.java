b9643dee46262e18dbaf66892fc6d3bc8dae0804
csseifms <csseifms@gmail.com>
2021-12-28 05:28:59 +0200

    many2one one2many

---
+++ src/com/musala/jdbc/CreateCourseReviewsDemo.java
@@ -0,0 +1,53 @@
+package com.musala.jdbc;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateCourseReviewsDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Review.class)
+				.addAnnotatedClass(Course.class).buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			// start a transaction
+			session.beginTransaction();
+
+//			Course course = new Course("Spring");
+//			
+//			course.addReview(new Review("awsome"));
+//			course.addReview(new Review("speedy"));
+//			course.addReview(new Review("details"));
+//			
+//			session.save(course);
+			
+			
+			Course course = session.get(Course.class, 10);
+
+			session.delete(course);
+			
+			System.out.println(course.toString());
+			
+			// commit transaction
+			session.getTransaction().commit();
+
+			System.out.println("Done!");
+		} finally {
+
+			// add clean up code
+			session.close();
+
+			factory.close();
+		}
+	}
+
+}
